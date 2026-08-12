888cc82b74f37297e9ac79abcb537af10e0fbf1d
iChit111 <studyayu012@gmail.com>
2026-08-13 02:04:46 +0530

    Fix question 5 code
    I've added the virtual keyword which was missing when inheriting from Person class. The showRole() is defined in both Employee and Student, therefore it was causing an ambiguity error in TeachingAssistant. To fix this I've added a scope resolution operator in TeachingAssistant to match the expected output. The answers to the 4 questions mentioned in q05-input.txt are written in q05-answers.txt

--- questions/question-05-teaching-assistant.cpp
+++ questions/question-05-teaching-assistant.cpp
@@ -23,7 +23,7 @@ public:
     }
 };
 
-class Student : public Person
+class Student : virtual public Person
 {
 protected:
     int rollNo;
@@ -44,7 +44,7 @@ public:
     }
 };
 
-class Employee : public Person
+class Employee : virtual public Person
 {
 protected:
     int employeeId;
@@ -79,7 +79,7 @@ public:
         int r,
         int id,
         int h)
-        : Student(n, r),
+        : Person(n), Student(n, r),
           Employee(n, id)
     {
         hours = h;
@@ -90,7 +90,7 @@ public:
     void display()
     {
         showPerson();
-        showRole();
+        Student::showRole();
 
         cout << "Working hours: "
              << hours << endl;
@@ -100,10 +100,10 @@ public:
 int main()
 {
     TeachingAssistant ta(
-        "Aarav",
-        124,
-        501,
-        12);
+        "Riya",
+        315,
+        712,
+        18);
 
     ta.display();
 
