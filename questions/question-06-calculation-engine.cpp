d81f3b7a55ca6b172f08fe0d85bfe7509a73ee1c
iChit111 <studyayu012@gmail.com>
2026-08-13 19:45:51 +0530

    Fix question 6 code
    The template function maximum was incorrectly returning the minimum of the two parameters. So I've changed line 57 to handle the conditional properly. I've also added a virtual destructor on line 25. The answers of the 5 test case questions are answered in q06-answers.txt

--- questions/question-06-calculation-engine.cpp
+++ questions/question-06-calculation-engine.cpp
@@ -53,7 +53,7 @@ public:
 template <class T>
 T maximum(T a, T b)
 {
-    if (a < b)
+    if (a > b)
         return a;
 
     return b;
@@ -66,8 +66,8 @@ int main()
     services[0] = new StudentService();
     services[1] = new EmployeeService();
 
-    double amount = 1000;
-    double rate = 10;
+    double amount = 500;
+    double rate = 20;
 
     try
     {
@@ -94,7 +94,7 @@ int main()
                     employeeAmount)
              << endl;
     }
-    catch (runtime_error e)
+    catch (invalid_argument e)
     {
         cout << "Runtime error: "
              << e.what() << endl;
